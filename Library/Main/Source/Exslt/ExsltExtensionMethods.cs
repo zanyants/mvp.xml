508b2d3f6723c0bd25d3417017f5d2e78c6a3535
Tom Glastonbury <tomg@zanyants.com>
2012-01-02 17:12:51 +0000

    Added an overload of ExsltEvaluate to allow prefixed namespaces to be provided.

--- Library/Main/Source/Exslt/ExsltExtensionMethods.cs
+++ Library/Main/Source/Exslt/ExsltExtensionMethods.cs
@@ -1,4 +1,6 @@
 ﻿using System.Xml.XPath;
+using System.Xml;
+using System.Collections.Generic;
 
 namespace Mvp.Xml.Exslt
 {
@@ -19,7 +21,32 @@ namespace Mvp.Xml.Exslt
             XPathExpression expr = nav.Compile( xpath );
             ExsltContext ctxt = new ExsltContext( nav.NameTable );
             expr.SetContext( ctxt );
+
             return nav.Evaluate( expr );
         }
+
+        /// <summary>
+        /// Evaluate an XPath expression on an <c>IXPathNavigable</c> instance, using the <c>ExsltContext</c> context.
+        /// </summary>
+        /// <param name="xpn">An <c>IXPathNavigable</c> instance.</param>
+        /// <param name="xpath">The XPath epression to be evaluated.</param>
+        /// <param name="namespaces">An array of prefix/URI pairs to be added as namespaces to the evaluation context.</param>
+        /// <returns>The result of the evaluation.</returns>
+        public static object ExsltEvaluate( this IXPathNavigable xpn, string xpath, KeyValuePair< string, string >[] namespaces )
+        {
+            XPathNavigator nav = xpn.CreateNavigator();
+            XPathExpression expr = nav.Compile( xpath );
+            ExsltContext ctxt = new ExsltContext( nav.NameTable );
+            
+            foreach ( var kvp in namespaces )
+            {
+                ctxt.AddNamespace( kvp.Key, kvp.Value );
+            }
+
+            expr.SetContext( ctxt );
+
+            return nav.Evaluate( expr );
+        }
+
     }
 }
