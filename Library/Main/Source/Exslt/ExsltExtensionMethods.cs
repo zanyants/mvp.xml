959f2a8dfeb2d6a1a136af5cba479936987b156b
Marko Zerdin <marko@zanyants.com>
2014-05-26 12:10:46 +0100

    Added a version of ExsltEvaluate that takes a compiled XPath expression.

--- Library/Main/Source/Exslt/ExsltExtensionMethods.cs
+++ Library/Main/Source/Exslt/ExsltExtensionMethods.cs
@@ -32,12 +32,26 @@ namespace Mvp.Xml.Exslt
         /// <param name="xpath">The XPath epression to be evaluated.</param>
         /// <param name="namespaces">An array of prefix/URI pairs to be added as namespaces to the evaluation context.</param>
         /// <returns>The result of the evaluation.</returns>
-        public static object ExsltEvaluate( this IXPathNavigable xpn, string xpath, KeyValuePair< string, string >[] namespaces )
+        public static object ExsltEvaluate( this IXPathNavigable xpn, string xpath, IEnumerable<KeyValuePair< string, string >> namespaces )
         {
             XPathNavigator nav = xpn.CreateNavigator();
             XPathExpression expr = nav.Compile( xpath );
+
+            return ExsltEvaluate( xpn, expr, namespaces );
+        }
+
+        /// <summary>
+        /// Evaluate an XPath expression on an <c>IXPathNavigable</c> instance, using the <c>ExsltContext</c> context.
+        /// </summary>
+        /// <param name="xpn">An <c>IXPathNavigable</c> instance.</param>
+        /// <param name="expr">XPath epression to be evaluated.</param>
+        /// <param name="namespaces">An array of prefix/URI pairs to be added as namespaces to the evaluation context.</param>
+        /// <returns>The result of the evaluation.</returns>
+        public static object ExsltEvaluate( this IXPathNavigable xpn, XPathExpression expr, IEnumerable<KeyValuePair<string, string>> namespaces )
+        {
+            XPathNavigator nav = xpn.CreateNavigator();
             ExsltContext ctxt = new ExsltContext( nav.NameTable );
-            
+
             foreach ( var kvp in namespaces )
             {
                 ctxt.AddNamespace( kvp.Key, kvp.Value );
@@ -47,6 +61,5 @@ namespace Mvp.Xml.Exslt
 
             return nav.Evaluate( expr );
         }
-
     }
 }
